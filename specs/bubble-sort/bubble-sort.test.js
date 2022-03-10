2cc89adb52d9e4ba96049f44916c65ea2680a28d
Yashwanth reddy <yash868686reddy@gmail.com>
2022-03-09 21:49:03 -0500

    Done With Bubble Sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,11 +12,23 @@
 
 function bubbleSort(nums) {
   // code goes here
+  let i = 0;
+  while (i < nums.length - 1) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+      }
+    }
+    i++;
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
--- src/Sort.js
+++ src/Sort.js
@@ -4,13 +4,26 @@ import { App, snapshot, done, clear } from "./sort-visualizer";
 
 import "./sort.css";
 
-function sort(array) {
+function sort(nums) {
   // do cool stuff here
 
   // call snapshot any time you do anything to the array
   // it's okay if you call it with duplicate value array,
   // it will deduplicate for you
-  snapshot(array);
+  let swapped = false;
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length - 1; i++) {
+      snapshot(nums);
+      if (nums[i] > nums[i + 1]) {
+        swapped = true;
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+      }
+    }
+  } while (swapped);
+  snapshot(nums);
 }
 
 export default function SortComponent() {
